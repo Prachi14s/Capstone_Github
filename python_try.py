e961dfc0ace5f3cf8e16262401c08ef70ac0a19e
Prachi14s <prachi.jindal005@gmail.com>
2019-03-29 21:57:44 +0530

    sync python script with gitbash

---
+++ python_try.py
@@ -0,0 +1,2 @@
+https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/274738bd-cbb8-4450-bf01-ce8fc0579857/view?access_token=05a5f7283f49e0587587c72f05f957d7a94166be7dc9be4a13aa650e433b87ai
+print("hello mr.")
